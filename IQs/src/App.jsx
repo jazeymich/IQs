9ca20aff33878cb5c6aefcd19f7178f5585a8b9a
jazeymich <josephkabera42@gmail.com>
2024-06-04 23:02:27 +0300

    first commit

---
+++ IQs/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Accordian from './components/accordion'
+
+function App() {
+
+  return (
+    <div className='App'>
+      {/* Accordian component */}
+      <Accordian />
+    </div>
+  )
+}
+
+export default App
